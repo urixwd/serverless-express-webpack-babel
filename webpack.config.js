e616df36fba70f9b3c1ff41deaf5bb8a7dd86446
Uri Michaeli <urixwd@gmail.com>
2017-12-15 12:15:35 +0100

    serverless-webpack

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const nodeExternals = require('webpack-node-externals');
+const path = require('path');
+
+module.exports = {
+  entry: './serverless.js',
+  target: 'node',
+  externals: [nodeExternals()],
+  output: {
+    libraryTarget: 'commonjs',
+    path: path.join(__dirname, '.webpack'),
+    filename: 'dist.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+      },
+    ],
+  },
+};
